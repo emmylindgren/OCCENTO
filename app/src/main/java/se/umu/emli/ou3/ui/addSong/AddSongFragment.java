baed1e5967560d3d629b74873b3de37ad32fbe15
Emmy Lindgren <id19eln@cs.umu.se>
2021-07-29 18:56:18 +0200

    Small things on add song fixed.

--- app/src/main/java/se/umu/emli/ou3/ui/addSong/AddSongFragment.java
+++ app/src/main/java/se/umu/emli/ou3/ui/addSong/AddSongFragment.java
@@ -1,18 +1,23 @@
 package se.umu.emli.ou3.ui.addSong;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import org.jetbrains.annotations.NotNull;
 
 import se.umu.emli.ou3.R;
 import se.umu.emli.ou3.Song;
@@ -23,6 +28,7 @@ public class AddSongFragment extends Fragment {
     private EditText editTextTitle;
     private EditText editTextArtist;
     private EditText editTextLyrics;
+    private TextView nrOfCharsLyrics;
     private Button saveSongButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -32,12 +38,8 @@ public class AddSongFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_add_song, container, false);
 
-        editTextTitle = root.findViewById(R.id.title_edit_text);
-        editTextArtist = root.findViewById(R.id.artist_edit_text);
-        editTextLyrics = root.findViewById(R.id.lyrics_edit_text);
-        saveSongButton = root.findViewById(R.id.save_song);
-
-        saveSongButton.setOnClickListener(this::addSong);
+        setUpViewItems(root);
+        setUpListeners();
 
         return root;
     }
@@ -45,8 +47,50 @@ public class AddSongFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-
     }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        hideKeyboard(requireView());
+    }
+
+    private void setUpListeners() {
+        saveSongButton.setOnClickListener(this::addSong);
+        editTextLyrics.addTextChangedListener(startTextWatcher());
+    }
+
+    @NotNull
+    private TextWatcher startTextWatcher() {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String currentText = editable.toString();
+                int currentLength = currentText.length();
+                nrOfCharsLyrics.setText(currentLength + "/100");
+            }
+        };
+    }
+
+    private void setUpViewItems(View root) {
+        editTextTitle = root.findViewById(R.id.title_edit_text);
+        editTextArtist = root.findViewById(R.id.artist_edit_text);
+        editTextLyrics = root.findViewById(R.id.lyrics_edit_text);
+        nrOfCharsLyrics = root.findViewById(R.id.max_chars_count);
+        saveSongButton = root.findViewById(R.id.save_song);
+    }
+
+
 
     private void addSong(View view){
         String title = editTextTitle.getText().toString();
@@ -74,7 +118,8 @@ public class AddSongFragment extends Fragment {
      * @param view
      */
     private void hideKeyboard(View view){
-        InputMethodManager imm = (InputMethodManager) requireContext().getSystemService(requireActivity().INPUT_METHOD_SERVICE);
+        InputMethodManager imm = (InputMethodManager) requireContext().
+                getSystemService(requireActivity().INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
